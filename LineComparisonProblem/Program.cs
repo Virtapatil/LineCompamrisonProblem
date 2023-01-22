e8435b12aeae173a7154857c610984e8ee95a4a1
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-22 22:08:31 +0530

    [Virta] Calculating The lenght of Line

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LineComparisonProblem
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
 {
-    internal class Program
+    public class program
     {
-        static void Main(string[] args)
+        public static void LenghtOfLine(int x1, int y1, int x2, int y2)
         {
-            Console.WriteLine("Hello, World!");
+            double result = (x2 - x1) ^ 2 + (y2 - y1) ^ 2;
+            double c = Math.Sqrt(result);
+            Console.WriteLine(c);
+        }
+        public static void Main(string[] args)
+        {
+            LenghtOfLine(1, 2, 3, 4);
         }
     }
 }
